cb28024b5751da8230440a70c8aa79f48f2055a1
Mads Alexander Fossdal <MadsAlexander.Fossdal@broadnet.no>
2021-01-20 17:50:38 +0100

    ok

---
+++ index.js
@@ -0,0 +1,9 @@
+if ("servicewWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW Registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW RegistereD Failed");
+        console.log(error);
+    })
+}
